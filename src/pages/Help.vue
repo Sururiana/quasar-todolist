e158dbc11281cd418180ce75d67415ca3c5d431f
Zainul <zainul.arif97.za@gmail.com>
2020-01-19 11:02:06 +0700

    Initial commit

---
+++ src/pages/Help.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Help</h5>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia dolore deleniti aut? Similique, dolorem eum consequuntur non dolor voluptate, totam ipsa suscipit mollitia recusandae optio tempora, minima saepe quasi eos.</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia beatae eos harum deleniti hic, recusandae ratione libero. In tempora sed ab nemo laborum corrupti libero cum qui quasi, voluptatem et?</p>
+  </q-page>
+</template>
+
+<script>
+export default {
+
+}
+</script>
